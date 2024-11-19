80914115f7efcf2a22a9bb2f49adc8bf4aecc1e9
emgroenewald <elliot.mills-groenewald@elev.ga.lbs.se>
2024-11-19 09:10:12 +0100

    Version Beta
    Game has Movement and Lighting

--- Scubby Dubby Blue/Assets/Script/HeadBob.cs
+++ Scubby Dubby Blue/Assets/Script/HeadBob.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class HeadBob : MonoBehaviour
 {
-    public Transform _camera;
-    public Transform hand;
-    public float cameraSensitivity = 200.0f;
-    public float cameraAcceleration = 5.0f;
-
-    private float rotation_x_axis;
-    private float rotation_y_axis;
-
-    [Header ("HeadBob")]
     [Range(0.001f, 0.01f)]
     public float Amount = 0.002f;
     [Range(1f, 30f)]
@@ -28,31 +19,13 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
-        UnityEngine.Cursor.visible = false;
-
         StartPos = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rotation_x_axis += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
-        rotation_y_axis += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-
-        transform.localRotation = Quaternion.Lerp(transform.localRotation, 
-            Quaternion.Euler(0, rotation_y_axis, 0), cameraAcceleration * Time.deltaTime);
-
-        _camera.localRotation = Quaternion.Lerp(_camera.localRotation,
-            Quaternion.Euler(-rotation_x_axis, 0, 0), cameraAcceleration * Time.deltaTime);
-
-        rotation_x_axis = Mathf.Clamp(rotation_x_axis, -90f, 90f);
-
-        hand.localRotation = Quaternion.Euler(-rotation_x_axis, rotation_y_axis, 0);
-
-
         CheckForHeadBobTrigger();
-
     }
 
     private void CheckForHeadBobTrigger()
@@ -69,7 +42,7 @@ public class CameraController : MonoBehaviour
     {
         Vector3 pos = Vector3.zero;
         pos.y += Mathf.Lerp(pos.y, Mathf.Sin(Time.time * Frequency) * Amount * 1.4f, Smooth * Time.deltaTime);
-        pos.x += Mathf.Lerp(pos.x, Mathf.Cos(Time.time * Frequency / 2f) * Amount * 1.6f, Smooth * Time.deltaTime);
+        pos.x += Mathf.Lerp(pos.x, Mathf.Cos(Time.time * Frequency /2f) * Amount * 1.6f, Smooth * Time.deltaTime);
         transform.localPosition += pos;
 
         return pos;
@@ -80,4 +53,7 @@ public class CameraController : MonoBehaviour
         if (transform.localPosition == StartPos) return;
         transform.localPosition = Vector3.Lerp(transform.localPosition, StartPos, 1 * Time.deltaTime);
     }
+
 }
+
+
